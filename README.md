f6280a6dc0dbc8fdd337cf542aacc1889819207f
AmateurECE <edtwardy@mtu.edu>
2019-03-05 18:01:39 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# The Stupid Bug Tracker #
+
+This tools is based off of [t](https://github.com/sjl/t). It searches the
+current git repository (and all subdirectories) for `TODO` comments in code. It
+then puts these comments into a file in the top level directory of the
+repository with the name `bugs`. It matches C/C++ and Bash style `TODO`
+comments of the form:
+
+```
+(#|/(\*|/))\s*TODO:\s*(.*)
+```
+
+When run without any arguments, it prints the bugs in the `bugs` file in the
+top level directory of the current git repository. If the current working
+directory is not in a git repository, `bugs.py` exits with an error. When run
+with `update` as the first argument, `bugs.py` will update the `bugs` file in
+the top level directory of the git repository containing the current working
+directory to contain the most up-to-date index of bugs.
+
+```
+bugs.py [update]
+```
+
+The script will not index `TODO` comments in the `.git/` directory by default,
+nor any files listed in the `.gitignore` (if it exists). If this is not enough
+control, the user may also have a file with the name `.bignore` in the top
+level directory of the git repository containing the current working directory
+that contains a list of more files and directories to ignore.
+
+I have it aliased to just `b` in my shell. It's a pretty neat tool, and it
+plays nicely with [Sysgit](https://github.com/AmateurECE/Sysgit).
---
+++ bugs.py
@@ -0,0 +1,161 @@
+#!/usr/bin/env python3
+"""A simple bug tracking tool."""
+###############################################################################
+# NAME:		    bugs.py
+#
+# AUTHOR:	    Ethan D. Twardy <edtwardy@mtu.edu>
+#
+# CREATED:	    02/28/2019
+#
+# LAST EDITED:	    03/01/2019
+###
+
+from pathlib import Path
+import os
+import sys
+import re
+
+from t import t
+
+###############################################################################
+# FUNCTIONS
+###
+
+class Bugs:
+    """Contains the logic of the `b' tool."""
+
+    # Static class members
+    # Matches C/C++ and Bash style comments
+    todoRegex = r'(#|/(\*|/))\s*TODO:\s*(.*)'
+    r_todo = re.compile(todoRegex)
+
+    def __init__(self, pwd):
+        self.gitDir = self.findRepositoryRoot(pwd)
+        self.bugDict = t.TaskDict(taskdir=self.gitDir, name='bugs')
+
+    @staticmethod
+    def findRepositoryRoot(pwd):
+        """
+        Determine if `pwd' is inside of a git repository and, if so, return the
+        path of the root directory of the repository.
+        """
+        gitHere = Path(os.fspath(pwd) + '/.git')
+        rootDir = os.path.abspath(os.sep) # Platform independent
+        while os.path.abspath(pwd) != rootDir:
+            if gitHere.is_dir():
+                return pwd
+            pwd = Path(os.fspath(pwd) + '/..')
+            gitHere = Path(os.fspath(pwd) + '/.git')
+        return None
+
+    @staticmethod
+    def readIgnoreFile(ignoreFilename):
+        """INTERNAL. Read ignoreFilename file for the list of regexes."""
+        try:
+            with open(ignoreFilename, 'r') as ignoreFile:
+                return [ln.rstrip('\n') for ln in ignoreFile.readlines()]
+        except FileNotFoundError:
+            pass
+
+    def buildFileList(self):
+        """INTERNAL. Builds the list of files to search for TODO comments."""
+        rejectRegexes = ['.git/'] # Automatically ignore .git/
+        # Read .bignore file, if it exists
+        rejectRegexes.extend(self.readIgnoreFile(self.gitDir + '/.bignore')
+                             or [])
+        # Read .gitignore file, if it exists
+        rejectRegexes.extend(self.readIgnoreFile(self.gitDir + '/.gitignore')
+                             or [])
+
+        # Walk the repository
+        #pylint: disable=unused-variable
+        validPaths = list()
+        for dirpath, dirnames, filenames in os.walk(self.gitDir):
+            # First, check if the dirpath contains something we're supposed to
+            # ignore.
+            for regex in rejectRegexes:
+                if regex in dirpath:
+                    continue
+
+            # Then, consider each file in the current directory.
+            for fn in filenames:
+                fn = dirpath + '/' + fn
+                reject = False
+                # If the filename matches any of the lines in .bignore,
+                # reject it.
+                for regex in rejectRegexes:
+                    if regex in fn:
+                        reject = True
+                if not reject:
+                    validPaths.append(fn)
+        return validPaths
+
+    def update(self):
+        """Update the bugs file in the gitDir directory."""
+        fileList = self.buildFileList()
+        # Remove the bugs file if it exists
+        try:
+            os.unlink(self.gitDir + '/bugs')
+        except FileNotFoundError:
+            pass
+
+        # Begin looking for bugs
+        for fn in fileList:
+            try:
+                bugs = self.getBugs(fn) or []
+                for bug in bugs:
+                    # Add the bug to the dictionary
+                    self.bugDict.add_task(fn + ': ' + bug)
+            except UnicodeDecodeError:
+                print(('Warning: Could not decode file "{}". If this is a '
+                       'binary file, consider adding it to your '
+                       '.bignore or .gitignore').format(fn), file=sys.stderr)
+                continue
+
+        # Finally, write the changes to disk
+        self.bugDict.write(delete_if_empty=True)
+
+    @staticmethod
+    def getBugs(fileName):
+        """Get a list of bugs in the file `fileName'"""
+        bugs = list()
+        with open(fileName, 'r') as inputFile:
+            for line in inputFile.readlines():
+                # Attempt to match the regex.
+                bug = Bugs.r_todo.search(line)
+                if bug is not None:
+                    # If we are matching C style comments, remove the ' */'
+                    if bug.group(2) == '*' and bug.group(3)[-3:] == ' */':
+                        bugs.append(bug.group(3)[:-3])
+                    else:
+                        bugs.append(bug.group(3))
+        return bugs
+
+    def printBugs(self):
+        """Print the bugs in the bugs file"""
+        self.bugDict.print_list()
+
+###############################################################################
+# MAIN
+###
+
+def main():
+    """Run bugs"""
+    ### Find the Git repository
+    bugs = Bugs('.')
+
+    # TODO: This is a test bug
+    try:
+        ### Perform b update
+        if sys.argv[1] == 'update':
+            bugs.update()
+        else:
+            print('fatal: command not understood')
+            return 1
+    except IndexError:
+        bugs.printBugs()
+
+if __name__ == '__main__':
+    main()
+
+##############################################################################
