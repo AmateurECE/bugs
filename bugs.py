AmateurECE/bugs

commit 8b012002c01d6a6823d59b64e0c96f657261ab6d

Fix bug, add support for LaTeX
Fix a bug that causes the script to fail when searching the `bugs`
file for TODO comments (because it was deleted). Also add support
for recognizing LaTeX TODO comments.

bugs.py
@@ -26,7 +26,7 @@ class Bugs:
 
     # Static class members
     # Matches C/C++ and Bash style comments
-    todoRegex = r'(#|/(\*|/))\s*TODO:\s*(.*)'
+    todoRegex = r'(%|#|/(\*|/))\s*TODO:\s*(.*)'
     r_todo = re.compile(todoRegex)
 
     def __init__(self, pwd):
@@ -59,7 +59,7 @@ class Bugs:
 
     def buildFileList(self):
         """INTERNAL. Builds the list of files to search for TODO comments."""
-        rejectRegexes = ['.git/'] # Automatically ignore .git/
+        rejectRegexes = ['.git/', 'bugs'] # Automatically ignore .git/ and bugs
         # Read .bignore file, if it exists
         rejectRegexes.extend(self.readIgnoreFile(self.gitDir + '/.bignore')
                              or [])

commit 041a6906783c5c2626d3d17a2e67833f869b746a

Fix bug introduced by 8b01200
Referenced commit introduced a bug that caused any file that contains
the substring 'bugs' to be rejected from the file list in function
Bugs.buildFileList(). Check for equality instead, then break loop.

bugs
@@ -1,2 +1,2 @@
-./bugs.py: This is a test bug | id:41444eac158f85ac92db5ae065cf55bb0266ac70
+./bugs.py: Make to work with emacs-lisp comments | id:49ad6e202eb9f81bc432b2ce3b70a39734fcf53d
 ./bugs.py: Implement glob matching for ignore file entries | id:9d5e1eca1ad4be070ce6d99dd892dfb153f98167

bugs.py
@@ -28,6 +28,7 @@ class Bugs:
 
     # Static class members
     # Matches C/C++ and Bash style comments
+    # TODO: Make to work with emacs-lisp comments
     todoRegex = r'(%|#|/(\*|/))\s*TODO:\s*(.*)'
     r_todo = re.compile(todoRegex)
 
@@ -74,7 +75,7 @@ class Bugs:
     def buildFileList(self):
         """INTERNAL. Builds the list of files to search for TODO comments."""
         # Automatically ignore .git/ and bugs
-        rejectRegexes = ['.git/', 'bugs', '~']
+        rejectRegexes = ['.git/', '~']
         # Read .bignore file, if it exists
         rejectRegexes.extend(self.readIgnoreFile(self.gitDir + '/.bignore')
                              or [])
@@ -99,12 +100,13 @@ class Bugs:
             for fn in filenames:
                 fn = dirpath + '/' + fn
                 reject = False
-                # If the filename matches any of the lines in .bignore,
+                # If the filename matches any of the lines in rejectRegexes,
                 # reject it.
                 # TODO: Implement glob matching for ignore file entries
                 for regex in rejectRegexes:
-                    if regex in fn:
+                    if regex in fn or fn == 'bugs':
                         reject = True
+                        break
                 if not reject:
                     validPaths.append(fn)
         return validPaths
@@ -167,7 +169,6 @@ def main():
     ### Find the Git repository
     bugs = Bugs('.')
 
-    # TODO: This is a test bug
     try:
         ### Perform b update
         if sys.argv[1] == 'update':